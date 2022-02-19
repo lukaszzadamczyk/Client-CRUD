1b09dfb5857c6dddb96349541d7e49deb683deac
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-02-19 14:00:08 +0000

    Create express config

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const {clientRouter} = require("./routers/client");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.engine('.hbs', hbs.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.use('/client', clientRouter);
+
+app.listen(3000, 'localhost', () => {
+    console.log('Listening on http://localhost:3000');
+})

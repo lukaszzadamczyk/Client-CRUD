19f16da4c670756a7dfda30660a28298ba366f1a
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-02-20 20:51:44 +0000

    Add new routers

--- routers/client.js
+++ routers/client.js
@@ -18,13 +18,56 @@ clientRouter
         })
     })
     .post('/', (req, res) => {
+        const {email, name, nextContactAt, notes} = req.body;
 
+        const id = db.create({
+            email,
+            name,
+            nextContactAt,
+            notes
+        })
+
+        res.render('client/added',{
+            name,
+            id,
+        })
     })
     .put('/:id', (req, res) => {
 
+        const {id} = req.params;
+
+        const {email, name, nextContactAt, notes} = req.body;
+
+        db.update(id, {
+            email,
+            name,
+            nextContactAt,
+            notes,
+        })
+
+        res.render('client/update', {
+            name,
+            id
+        })
     })
     .delete('/:id', (req, res) => {
 
+        const {id} = req.params;
+
+        db.delete(id);
+
+        res.render('client/deleted')
+    })
+    .get('/form/add', (req, res) => {
+        res.render('client/form/add');
+    })
+    .get('/form/edit/:id', (req, res) => {
+
+        const {id} = req.params
+
+        res.render('client/form/edit',{
+            client: db.getOne(id),
+        });
     })
 
 module.exports = {
